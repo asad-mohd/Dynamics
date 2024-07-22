7a5f1e803ddfb10e8691dadee7d716a305efdc02
Asad <asad-mohd@tftus.com>
2024-07-22 16:50:27 +0530

    Fix pipeline

--- src/tests/check.spec.ts
+++ src/tests/check.spec.ts
@@ -4,14 +4,14 @@ import * as OTPAuth from "otpauth";
 const username: any = process.env.USER_NAME;
 const password: any = process.env.PASSWORD;
 
-test(`Create contact`, async ({ page }) => {
+test(`check-microsoft-login`, async ({ page }) => {
     // const { baseURL } :  any = config.projects[1].use;
     // console.log(baseURL)
     let totp = new OTPAuth.TOTP({
       issuer: "Microsoft",
       secret: "c6cpxv2ptyk7fyt7",
     });
-    await page.goto('/');
+    await page.goto(`/main.aspx?appid=003ada10-09b4-ea11-a812-000d3a7ed588`);
     await page.locator('[type="email"]').fill(username);
     await page.locator('[type="submit"]').click();
     // expect(page.locator('[id="contentWrapper"]')).toBeVisible({timeout: 60000});
@@ -26,7 +26,7 @@ test(`Create contact`, async ({ page }) => {
       throw e;
     }
     await page.locator('//*[@id="passwordArea"]').click();
-    await page.locator('[id="passwordInput"]').fill("Healithcare24");
+    await page.locator('[id="passwordInput"]').fill("Healthcare24");
     await page.locator('[id="submitButton"]').click();
     await page
       .getByText(`I can't use my Microsoft Authenticator app right now`)
